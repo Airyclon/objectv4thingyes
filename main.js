e25a4b0aba7287ae2554c6f7eda1b553ccd72b83
Airyclon <84323073+Airyclon@users.noreply.github.com>
2022-08-31 16:41:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+video = "";
+status = "";
+object = "";
+objects = [];
+
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++){
+            if(object[i].label == object)
+            {
+                document.getElementById("status").innerHTML = "Status: Object Detected";
+                document.getElementById("number_of_objects").innerHTML = "Number Of Objects are: " + objects.length;
+    
+                fill("FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+            else
+            {
+                document.getElementById("status").innerHTML = "Status: Object Not Detected";
+                document.getElementById("number_of_objects").innerHTML = "Number Of Objects are: " + objects.length;
+    
+                fill("FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+    }
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status- Detecting Objects";
+    object = document.getElementById("input").value;
+    console.log(object);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+        console.log(results);
+        objects = results;
+}
